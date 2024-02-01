5ef1a886e5a451dbe9716169323eb3fe6b55bfc3
ArunKumar J <arunkumar22112003@gmail.com>
2024-02-02 01:04:04 +0530

    Delete haskel1.hs

--- haskel1.hs
+++
@@ -1,9 +0,0 @@
--- Define a function to calculate factorial
-factorial :: Integer -> Integer
-factorial 0 = 1
-factorial n = n * factorial (n - 1)
-
--- Use the function
-main :: IO ()
-main = do
-    print (factorial 5)
